d9fc224d0051cdc3f1747b4da791e3c2aa814fb9
yasir-rtx <yelfvankarlenstain@gmail.com>
2020-10-05 21:23:47 +0700

    Memasuki part 2, dimana belaajr syntax dalam php

---
+++ 02-Syntax_php/index.php
@@ -0,0 +1,15 @@
+<?php  
+
+// Pertemuan 2
+// syntax dalam php
+
+// Standar Output :
+// # echo
+	// echo '#echo --> Hello Yasir';
+// # print
+	// print '#print --> Hello Print';
+// # print_r
+	// print_r("Hello print_r")
+// # var_dump
+	var_dump('expression');
+?>
