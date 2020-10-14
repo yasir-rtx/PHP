7b2cad697f378830b06941daa5142b63dbd23519
yasir-rtx <yelfvankarlenstain@gmail.com>
2020-10-14 10:07:41 +0700

    Menambahkan percabangan dalam looping, agar baris genap bewarna lime

--- 03-Control_Flow/latihan2.php
+++ 03-Control_Flow/latihan2.php
@@ -4,20 +4,26 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Latihan 2</title>
+	<style type="text/css">
+		.genap {
+			background-color: lime;
+		}
+	</style>
 </head>
 <body>
 	<center>
 		<table border="1" cellpadding="10" cellspacing="0">
-		<?php 
-			for ($i = 1; $i <= 3; $i++) {
-				<?php ?>
-				echo '<tr>';
-				for ($j = 1; $j <= 5; $j++) {
-					echo "<td>$i,$j</td>";
-				}
-				echo '</tr>';
-			}
-		 ?>
+		<?php for ( $i = 1; $i <=10; $i++ ) : ?>
+			<?php if ( $i % 2 == 1) : ?>
+				<tr class="genap">
+			<?php else : ?>
+				<tr>
+			<?php endif; ?>
+				<?php for ( $j = 1; $j <=5; $j++ ) : ?>
+					<td><?= "$i,$j" ?></td>
+				<?php endfor; ?>
+			</tr>
+		<?php endfor; ?>
 		</table>
 	</center>
 </body>
