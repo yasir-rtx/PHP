8920621e5833b5436c6cf6686f0f9b955b0e4d5e
yasir-rtx <yelfvankarlenstain@gmail.com>
2020-10-14 10:31:59 +0700

    memperbarui percabangan pada latihan3.php, jumlahkan $i dan $j kemudian hasil modulusnya digunakan untuk menentukan apakah cell itu genap atau bukan, kalau genap beri class hitam pada td

--- 03-Control_Flow/latihan3.php
+++ 03-Control_Flow/latihan3.php
@@ -12,20 +12,17 @@
 </head>
 <body>
 	<center>
-		<table border="1" cellpadding="35" cellspacing="0">
+		<table border="1" cellpadding="25" cellspacing="0">
 			<?php for ( $i=1; $i <= 8; $i++ ) : ?> <!--baris-->
-				<?php if ( $i % 2 == 1) : ?>
-					<tr class="black">
-				<?php else : ?>
-					<tr>
-				<?php endif; ?>
+				<tr>
 					<?php for ( $j =1; $j <=8; $j++ ) : ?> <!-- kolom-->
-						<?php if ( $j % 2 == 0) : ?>
+						<?php $x = $i + $j; ?>
+						<?php if ($x % 2 == 1) : ?>
 							<td class="black">
 						<?php else : ?>
 							<td>
 						<?php endif; ?>
-						</td>
+					</td>
 					<?php endfor; ?>
 				</tr>
 			<?php endfor; ?>
